87003bde49d18cb0a2582666cd166d2bf4d49a53
Emmanuelsam1 <Emmanuelsamuel415@gmail.com>
2023-10-17 21:55:59 -0700

    pointer

--- print_pointer.c
+++ print_pointer.c
@@ -10,6 +10,7 @@ int print_pointer(va_list val)
 {
 	void *p;
 	char *s = "(nil)";
+	char *hex_buffer;
 	long int a;
 	int i, b;
 
@@ -23,6 +24,19 @@ int print_pointer(va_list val)
 	a = (unsigned long int)p;
 	_putchar('0');
 	_putchar('x');
-	b = print_hex_extra(a);
+
+
+	hex_buffer = malloc(17);
+	if (hex_buffer == NULL)
+		return (-1);
+	snprintf(hex_buffer, 17, "%0161x", a);
+
+	for (i = 0; hex_buffer[i] != '\0'; i++)
+	{
+		_putchar(hex_buffer[i]);
+	}
+	free(hex_buffer);
+	b = i;
+
 	return (b + 2);
 }
